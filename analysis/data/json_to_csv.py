b25e63cb69f9f5ceaf0aefad0480e5c409fcb26c
jackiekazil <jackiekazil@gmail.com>
2015-05-02 22:53:02 -0400

    Adding json to csv conversion & sample files.

--- analysis/data/json_to_csv.py
+++ analysis/data/json_to_csv.py
@@ -13,9 +13,20 @@ import csv
 import json
 import os
 import sys
+import time
+
+from dateutil import parser
 
 src = sys.argv[1]
 out = sys.argv[2]
+
+
+def convert_date(date):
+    ''' Example input 'Mon Apr 27 22:35:53 +0000 2015' '''
+
+    parsed_date = parser.parse(date)
+    parsed_date = time.mktime(date.timetuple())
+    return parsed_date
 
 for the_file in os.listdir(src):
     file_path = os.path.join(src, the_file)
@@ -39,11 +50,70 @@ for the_file in os.listdir(src):
     os.makedirs(out, exist_ok=True)
     output = csv.writer(open(new_file_path, 'w'))
 
-    print(data[0].keys())
-    output.writerow(data[0].keys())
-    for rec in data:
-        output.writerow(rec.values())
+    keys = [
+        'id_str',
+        'coor_lat', 'coor_long', 'coorpt', 'coor_created', 'coor_whole',
+        'geo_lat', 'geo_long', 'geo_type',
+        'bbox', 'bbox_type', 'bbox_full_name', 'bbox_name', 'bbox_place_type',
+        'timestamp_ms',
+        'user_geo_able', 'user_id', 'user_location'
+    ]
+    output.writerow(keys)
 
-    output.close()
+    for rec in data:
+        import pprint
+        pprint.pprint(rec)
+
+        id_str = rec.get('id_str')
+
+        coor = coor_lat = coor_long = None
+        coorpt = coor_created = coor_whole = None
+        coor = rec.get('coordinates')
+        if coor:
+            coordinates = coor.get('coordinates')
+            if coordinates:
+                coor_lat = coordinates[0]
+                coor_long = coordinates[1]
+
+            coorpt = coor.get('type')
+            coor_created = coor.get('created_at')
+            coor_whole = coor.get('created_at')
+            if coor_whole:
+                coor_whole = convert_date(coor_whole)
+
+        geo = geo_lat = geo_long = None
+        geo = rec.get('geo')
+        if geo:
+            geo_coor = geo.get('coordinates')
+            if geo_coor:
+                geo_lat = geo_coor[0]
+                geo_long = geo_coor[1]
+
+            geo_type = geo.get('type')
+
+        bbox = rec['place']['bounding_box'].get('coordinates')
+        bbox_type = rec['place']['bounding_box'].get('type')
+        bbox_full_name = rec['place']['bounding_box'].get('full_name')
+        bbox_name = rec['place']['bounding_box'].get('name')
+        bbox_place_type = rec['place']['bounding_box'].get('place_type')
+
+        timestamp_ms = rec['timestamp_ms']
+
+        user_geo_able = rec['user']['geo_enabled']
+        user_id = rec['user']['id']
+        user_location = rec['user']['location']
+
+        row = [
+            id_str,
+            coor_lat, coor_long, coorpt, coor_created, coor_whole,
+            geo_lat, geo_long, geo_type,
+            bbox, bbox_type, bbox_full_name, bbox_name, bbox_place_type,
+            timestamp_ms,
+            user_geo_able, user_id, user_location
+        ]
+
+        row = [str(x) for x in row]
+        print(row)
+        output.writerow(row)
 
     print(file_path)
